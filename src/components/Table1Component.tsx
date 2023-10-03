61f76311bf007aac9daff481aaa19d677c928603
chaminju927 <minjoo92718@gmail.com>
2023-10-03 19:02:36 +0900

    .

--- src/components/Table1Component.tsx
+++ src/components/Table1Component.tsx
@@ -56,116 +56,157 @@ function Table1Component(): JSX.Element {
     return <SearchIcon />;
   };
   return (
-    <div>
-      <header id="notice">
+    <div
+      className="container"
+      style={{ display: "flex", flexDirection: "column" }}
+    >
+      <header>
         <div className="floatL">
-          <span>공지</span>
-          <span>2023-03-03 제이니스 근무 수칙</span>
-        </div>
-        <div className="floatR"></div>
+          <span className="notice">공지</span>
+          <ul>
+            {/* <ul style="display:inline-block;width:400px;margin-left:5px;">
+             */}
 
-        {/* <ul className="floatR">
-          <li>
-            <h2>제이니스</h2>
-          </li>
-          <li></li>
-        </ul> */}
+            <li>
+              <span className="ellipsis">
+                2023-03-03&nbsp;·&nbsp;제이니스 재택근무 수칙
+              </span>
+            </li>
+          </ul>
+        </div>
+        <ul className="floatR">
+          <h2 className="companyName">제이니스</h2>
+
+          {/* <li>
+            <button
+              type="button"
+              className="dtlView"
+              id="noticeActive"
+            ></button>
+          </li> */}
+
+          {/* <li className="selectLang">
+            <a href="javascript:void(0)">한국어</a>
+
+            <ul>
+              <li>
+                <a href="javascript:goLang('ko');" lang="ko">
+                  한국어
+                </a>
+              </li>
+              <li>
+                <a href="javascript:goLang('en');" lang="en">
+                  English
+                </a>
+              </li>
+              <li>
+                <a href="javascript:goLang('ja');" lang="ja">
+                  日本語
+                </a>
+              </li> 
+            </ul>
+          </li>*/}
+        </ul>
       </header>
 
-      <h2>출장관리</h2>
-      <table className="tableWrap bgBox w100">
-        <tr>
-          <td>
-            <div className="checklist">
-              <div className="formRadio">
-                <RadioGroup defaultValue="all" style={{ flexDirection: "row" }}>
-                  <FormControlLabel
-                    value="all"
-                    control={<RadioBtn />}
-                    label="전체"
-                  />
-                  <FormControlLabel
-                    value="normal"
-                    control={<RadioBtn />}
-                    label="정상"
-                  />
-                  <FormControlLabel
-                    value="cancel"
-                    control={<RadioBtn />}
-                    label="취소"
-                  />
-                </RadioGroup>
+      <form>
+        <h2 className="mainH2">출장관리</h2>
+        <table className="tableWrap bgBox w100">
+          <tr>
+            <td>
+              <div className="checklist">
+                <div className="formRadio">
+                  <RadioGroup
+                    defaultValue="all"
+                    style={{ flexDirection: "row" }}
+                  >
+                    <FormControlLabel
+                      value="all"
+                      control={<RadioBtn />}
+                      label="전체"
+                    />
+                    <FormControlLabel
+                      value="normal"
+                      control={<RadioBtn />}
+                      label="정상"
+                    />
+                    <FormControlLabel
+                      value="cancel"
+                      control={<RadioBtn />}
+                      label="취소"
+                    />
+                  </RadioGroup>
+                </div>
               </div>
-            </div>
-          </td>
-          <td className="txtCenter">
-            <div className="dateControl">
-              <input type="text" placeholder="YYYY.MM.DD" title="시작일" /> ~
-              <input type="text" placeholder="YYYY.MM.DD" title="종료일" />
-              <button className="btns bgBlue search">검색</button>
-            </div>
-          </td>
-          <td className="txtRight">
-            <div>
-              <button
-                onClick={() => {
-                  window.open(
-                    "/new",
-                    "new",
-                    "scrollbars=yes,resizable=no width=780 height=650, left=500,top=100"
-                  );
-                }}
-                className="btnRound bgGreen"
-              >
-                출장신청
-              </button>
-            </div>
-          </td>
-        </tr>
-      </table>
+            </td>
+            <td className="txtCenter">
+              <div className="dateControl">
+                <input type="text" placeholder="YYYY.MM.DD" title="시작일" /> ~
+                <input type="text" placeholder="YYYY.MM.DD" title="종료일" />
+                <button className="btns bgBlue search">검색</button>
+              </div>
+            </td>
+            <td className="txtRight">
+              <div>
+                <button
+                  onClick={() => {
+                    window.open(
+                      "/new",
+                      "new",
+                      "scrollbars=yes,resizable=no width=780 height=650, left=500,top=100"
+                    );
+                  }}
+                  className="btnRound bgGreen"
+                >
+                  출장신청
+                </button>
+              </div>
+            </td>
+          </tr>
+        </table>
 
-      <div className="tableTop">
-        <p>총 0건/ 0페이지</p>
-      </div>
-      <div className="w100" style={{ marginLeft: "10%" }}>
-        <TableContainer>
-          <Table
-            className="table horizontal"
-            sx={{ minWidth: 650, width: "100%" }}
-          >
-            <TableHead>
-              <TableRow>
-                <TableCell>구분</TableCell>
-                <TableCell align="right">유형</TableCell>
-                <TableCell align="right">시작일시</TableCell>
-                <TableCell align="right">종료일시</TableCell>
-                <TableCell align="right">사유</TableCell>
-                <TableCell align="right">등록일시</TableCell>
-                <TableCell align="right">취소일시</TableCell>
-                <TableCell align="right">상태</TableCell>
-                <TableCell align="right">결재</TableCell>
-                <TableCell align="right">관리</TableCell>
-                <TableCell align="right">결재상세</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow>
-                <TableCell colSpan={11}> 조회된 데이터가 없습니다</TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </div>
-
-      <div className="tablePage">
-        <div id="paging">
-          <select id="selectView">
-            <option value="10">10개씩</option>
-            <option value="20">20개씩</option>
-            <option value="30">30개씩</option>
-          </select>
+        <div className="w100" style={{ marginLeft: "10%" }}>
+          <div className="tableTop">
+            <p>총 0건/ 0페이지</p>
+          </div>
+          <TableContainer>
+            <Table
+              className="table horizontal"
+              sx={{ minWidth: 650, width: "100%" }}
+            >
+              <TableHead>
+                <TableRow>
+                  <TableCell>구분</TableCell>
+                  <TableCell align="right">유형</TableCell>
+                  <TableCell align="right">시작일시</TableCell>
+                  <TableCell align="right">종료일시</TableCell>
+                  <TableCell align="right">사유</TableCell>
+                  <TableCell align="right">등록일시</TableCell>
+                  <TableCell align="right">취소일시</TableCell>
+                  <TableCell align="right">상태</TableCell>
+                  <TableCell align="right">결재</TableCell>
+                  <TableCell align="right">관리</TableCell>
+                  <TableCell align="right">결재상세</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell colSpan={11}> 조회된 데이터가 없습니다</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <div className="tablePage">
+            <div id="paging">
+              <select id="selectView">
+                <option value="10">10개씩</option>
+                <option value="20">20개씩</option>
+                <option value="30">30개씩</option>
+              </select>
+            </div>
+          </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
